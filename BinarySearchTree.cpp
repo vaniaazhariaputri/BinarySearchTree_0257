eab8a4a796153ca9d61d3d942b82110b153406c5
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-06-26 10:56:32 +0700

    Menambahkan method postorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -108,3 +108,20 @@ public:
     inorder(ptr->rightchild);
 }
 
+void postorder(Node *ptr)
+{
+    if (isEmpty())
+    {
+        cout << "Tree is empty" << endl;
+        return;
+    }
+    
+    if (ptr == nullptr)
+        return;
+
+    postorder(ptr->leftchild);
+    postorder (ptr->rightchild);
+    cout <<  ptr->info <<  " "; //parent
+}
+};
+
