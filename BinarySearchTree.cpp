1278c06cf48953d70044b30a4f8abdffcaec3244
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-06-26 10:07:07 +0700

    Menambahkan constructor node class

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -4,5 +4,14 @@ using namespace std;
 class Node 
 {
 public:
-    
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Construvtor for the node class
+    Node()
+    {
+        leftchild = nullptr; // Intialieze left hild to null
+        right = nullptr; // Intialieze right child to null
+    }
 };
