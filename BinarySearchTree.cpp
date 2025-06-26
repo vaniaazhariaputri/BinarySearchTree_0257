06adec4cfbee14d9635132d5f964e4c6f0730f23
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-06-26 10:22:56 +0700

    Menambahkan setp 3 pada fungsi dengan node baru point null

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -38,5 +38,11 @@ public:
         // Step 2: Assign value to the data field of new node
         newNode->info = x;
 
-    }
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        
+
+    } 
 };
